167477c915c12de650590fbbe2576c0027629523
ErickMedinaBarrera <111623569+ErickMedinaBarrera@users.noreply.github.com>
2023-02-21 21:08:55 -0600

    Update PID.py

--- server/PID.py
+++ server/PID.py
@@ -1,9 +1,4 @@
-#! /usr/bin/python
-# File name   : car_dir.py
-# Description : By controlling Servo,thecamera can move Up and down,left and right and the Ultrasonic wave can move to left and right.
-# Website     : www.gewbot.com
-# Author      : William
-# Date        : 2018/08/22
+
 import time
 
 
